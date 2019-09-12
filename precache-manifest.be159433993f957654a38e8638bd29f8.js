33971ab6ab446635ea2c7cff856aa701e95d49a8
Bishwas Thapa <bishwastmgr@gmail.com>
2019-09-12 20:10:00 +0545

    Updates

---
+++ precache-manifest.be159433993f957654a38e8638bd29f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fde412d1310d9c46c7ea8ad00356a0b",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "59273b69b3240c6546d6",
+    "url": "/myapp/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f44fc08600d6f9a30f85",
+    "url": "/myapp/static/js/2.45f86b0b.chunk.js"
+  },
+  {
+    "revision": "59273b69b3240c6546d6",
+    "url": "/myapp/static/js/main.b7bb3f36.chunk.js"
+  },
+  {
+    "revision": "ad9b823830a0b1917a76",
+    "url": "/myapp/static/js/runtime~main.6d6f3a16.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
