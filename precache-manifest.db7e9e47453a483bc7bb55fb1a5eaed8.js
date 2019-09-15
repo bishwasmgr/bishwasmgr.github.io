2b6ed31dd7b14fdcfc748ee2873314fc9031525b
Bishwas Thapa <bishwastmgr@gmail.com>
2019-09-16 00:00:33 +0545

    Updates

---
+++ precache-manifest.db7e9e47453a483bc7bb55fb1a5eaed8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2fb3bca147f73736968cd9f5f0e3370",
+    "url": "/bishwasmgr.github.io/index.html"
+  },
+  {
+    "revision": "2cddb7ef8eeb48bdb45d",
+    "url": "/bishwasmgr.github.io/static/css/main.334e67e7.chunk.css"
+  },
+  {
+    "revision": "fb5ae75bd03601c580a2",
+    "url": "/bishwasmgr.github.io/static/js/2.da31070e.chunk.js"
+  },
+  {
+    "revision": "2cddb7ef8eeb48bdb45d",
+    "url": "/bishwasmgr.github.io/static/js/main.4578bf28.chunk.js"
+  },
+  {
+    "revision": "b1e88f798a00c0fdba4c",
+    "url": "/bishwasmgr.github.io/static/js/runtime~main.8fe78d13.js"
+  }
+]);
